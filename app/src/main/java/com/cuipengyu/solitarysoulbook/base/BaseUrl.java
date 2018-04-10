f4ecf8d5be24dc560e65637820129bbc9ed5cee5
崔鹏宇 <cuipengyusoul@gmail.com>
2018-04-10 18:37:18 +0800

    2018 04 10 18.30

--- app/src/main/java/com/cuipengyu/solitarysoulbook/base/BaseUrl.java
+++ app/src/main/java/com/cuipengyu/solitarysoulbook/base/BaseUrl.java
@@ -1,5 +1,6 @@
 package com.cuipengyu.solitarysoulbook.base;
 
 public  class BaseUrl {
-    public static final String baseUrl="http://api.zhuishushenqi.com/";
+    public static final String Api_baseUrl="http://api.zhuishushenqi.com/";
+    public static final String http_baseUrl="http://www.zhuishushenqi.com/";
 }
