df14bfa316e5955298aae02d41963247213f581d
崔鹏宇 <cuipengyusoul@gmail.com>
2018-04-11 21:58:24 +0800

    失败的封装 改进

---
+++ app/src/main/java/com/cuipengyu/solitarysoulbook/SelectBean.java
@@ -0,0 +1,73 @@
+package com.cuipengyu.solitarysoulbook;
+
+import com.cuipengyu.solitarysoulbook.base.BaseBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Create by    ： 崔鹏宇
+ * Time  is     ： 2018/4/11
+ * Email        ： cuipengyusoul@gmail.com
+ * Github       ： https://github.com/SolitarySoul
+ * Instructions ： 精选
+ */
+public class SelectBean extends BaseBean {
+    private List<SelectBook> selectBooks_List;
+    public List<SelectBook> getSelectBooks_List() {
+        if (selectBooks_List == null) return new ArrayList<>();
+        return selectBooks_List;
+    }
+
+    public void setSelectBooks_List(List<SelectBook> selectBooks_List) {
+        this.selectBooks_List = selectBooks_List;
+    }
+
+    public static class SelectBook {
+        private String img_link;//图片链接
+        private String name; //书名
+        private String book_Id;
+        private String author_Statua;//作者和状态
+        private String Introduction; //简介
+
+        public String getImg_link() {
+            return img_link == null ? "" : img_link;
+        }
+
+        public void setImg_link(String img_link) {
+            this.img_link = img_link;
+        }
+
+        public String getName() {
+            return name == null ? "" : name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getBook_Id() {
+            return book_Id == null ? "" : book_Id;
+        }
+
+        public void setBook_Id(String book_Id) {
+            this.book_Id = book_Id;
+        }
+
+        public String getAuthor_Statua() {
+            return author_Statua == null ? "" : author_Statua;
+        }
+
+        public void setAuthor_Statua(String author_Statua) {
+            this.author_Statua = author_Statua;
+        }
+
+        public String getIntroduction() {
+            return Introduction == null ? "" : Introduction;
+        }
+
+        public void setIntroduction(String introduction) {
+            Introduction = introduction;
+        }
+    }
+}
--- app/src/main/java/com/cuipengyu/solitarysoulbook/activitys/MainActivity.java
+++ app/src/main/java/com/cuipengyu/solitarysoulbook/activitys/MainActivity.java
@@ -1,39 +1,31 @@
 package com.cuipengyu.solitarysoulbook.activitys;
 
 
-import android.content.Intent;
-import android.util.Log;
 import android.view.View;
 
 import com.cuipengyu.solitarysoulbook.R;
+import com.cuipengyu.solitarysoulbook.SelectBean;
 import com.cuipengyu.solitarysoulbook.base.BaseActivity;
-import com.cuipengyu.solitarysoulbook.model.bean.MixTocBeanCon;
-import com.cuipengyu.solitarysoulbook.model.httphelper.HttpEngine;
-import com.cuipengyu.solitarysoulbook.model.httphelper.RetrofitHelper;
+import com.cuipengyu.solitarysoulbook.model.httphelper.JsoupEngine;
+import com.cuipengyu.solitarysoulbook.model.httphelper.JsoupHelper;
 import com.cuipengyu.solitarysoulbook.utils.ApplicationContextUtil;
-import com.cuipengyu.solitarysoulbook.utils.LogUtils;
 import com.squareup.leakcanary.RefWatcher;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-
-import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends BaseActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_watch:
-                Intent intent = new Intent(this, Main2Activity.class);
-                startActivity(intent);
+                JsoupHelper.getHelper().getHtmlStringData("bzrt", 1, new JsoupEngine.CallBack<SelectBean>() {
+                    @Override
+                    public void onSucces(SelectBean baseBean) {
+
+                    }
+
+                    @Override
+                    public void onError(String error) {
+
+                    }
+                });
 //                LoadingShow();
                 break;
             case R.id.btn_disss:
