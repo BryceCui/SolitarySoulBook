8addeea9be41555f5f91ebed8a51d7080b4a7d66
崔鹏宇 <cuipengyusoul@gmail.com>
2018-05-10 11:31:15 +0800

    2018 05 09

--- app/src/main/java/com/cuipengyu/solitarysoulbook/base/BaseViewHolder.java
+++ app/src/main/java/com/cuipengyu/solitarysoulbook/base/BaseViewHolder.java
@@ -1,9 +1,21 @@
 package com.cuipengyu.solitarysoulbook.base;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.ColorInt;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.IdRes;
+import android.support.annotation.StringRes;
 import android.support.v7.widget.RecyclerView;
 import android.util.SparseArray;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.cuipengyu.solitarysoulbook.R;
 
 /**
  * Create by    ： 崔鹏宇
@@ -14,17 +26,92 @@ import android.view.View;
  */
 public class BaseViewHolder extends RecyclerView.ViewHolder {
     private SparseArray<View> mViewArray;
+
     public BaseViewHolder(View itemView) {
         super(itemView);
-        this.mViewArray=new SparseArray<>();
+        this.mViewArray = new SparseArray<>();
     }
-    //绑定控件
-    public <T extends View> T getView(@IdRes int Ids){
-        View view=mViewArray.get(Ids);
-        if (view==null){
-            view=itemView.findViewById(Ids);
-            mViewArray.put(Ids,view);
+
+    public BaseViewHolder setText(@IdRes int viewId, CharSequence value) {
+        TextView textView = getView(viewId);
+        textView.setText(value);
+        return this;
+    }
+
+    public <T extends View> T getView(@IdRes int viewId) {
+        View view = mViewArray.get(viewId);
+        if (view == null) {
+            view = itemView.findViewById(viewId);
+            mViewArray.put(viewId, view);
         }
         return (T) view;
     }
+
+    public BaseViewHolder setText(@IdRes int viewId, @StringRes int strId) {
+        TextView textView = getView(viewId);
+        textView.setText(strId);
+        return this;
+    }
+
+    public BaseViewHolder setTextColor(@IdRes int viewId, @ColorInt int textColor) {
+        TextView view = getView(viewId);
+        view.setTextColor(textColor);
+        return this;
+    }
+
+    public BaseViewHolder setImageResource(@IdRes int viewId, @DrawableRes int imageResId) {
+        ImageView view = getView(viewId);
+        view.setImageResource(imageResId);
+        return this;
+    }
+
+    public BaseViewHolder setBackgroundColor(@IdRes int viewId, @ColorInt int color) {
+        View view = getView(viewId);
+        view.setBackgroundColor(color);
+        return this;
+    }
+
+    public BaseViewHolder setBackgroundRes(@IdRes int viewId, @DrawableRes int backgroundRes) {
+        View view = getView(viewId);
+        view.setBackgroundResource(backgroundRes);
+        return this;
+    }
+
+    public BaseViewHolder setImageDrawable(@IdRes int viewId, Drawable drawable) {
+        ImageView view = getView(viewId);
+        view.setImageDrawable(drawable);
+        return this;
+    }
+
+    public BaseViewHolder setImageBitmap(@IdRes int viewId, Bitmap bitmap) {
+        ImageView view = getView(viewId);
+        view.setImageBitmap(bitmap);
+        return this;
+    }
+
+    public BaseViewHolder setAlpha(@IdRes int viewId, float value) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            getView(viewId).setAlpha(value);
+        } else {
+            AlphaAnimation alpha = new AlphaAnimation(value, value);
+            alpha.setDuration(0);
+            alpha.setFillAfter(true);
+            getView(viewId).startAnimation(alpha);
+        }
+        return this;
+    }
+
+    public BaseViewHolder setGone(@IdRes int viewId, boolean visible) {
+        View view = getView(viewId);
+        view.setVisibility(visible ? View.VISIBLE : View.GONE);
+        return this;
+    }
+
+    public BaseViewHolder setVisible(@IdRes int viewId, boolean visible) {
+        View view = getView(viewId);
+        view.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
+        return this;
+    }
+
+
 }
